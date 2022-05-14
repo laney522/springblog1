89128da6d0969cbde844822d638e987d951c3ffa
laney522 <pm72222@gmail.com>
2022-05-14 23:45:36 +0900

    http request test2

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 사용자가 요청 -> 응답(Date)
@@ -12,8 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HttpControllerTest {
 	
 	@GetMapping("/http/get")
-	public String getTest() {
-		return "get request";
+	public String getTest(@RequestParam int id, @RequestParam String username) {
+		
+		return "get request : "+ id+", "+username;
 	}
 	
 	@PostMapping("/http/post")
