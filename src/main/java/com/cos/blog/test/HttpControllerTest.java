31e2612300844f58ccf0f860d5abea82769c2365
laney522 <pm72222@gmail.com>
2022-05-17 20:30:38 +0900

    http test4

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +19,8 @@ public class HttpControllerTest {
 	}
 	
 	@PostMapping("/http/post")
-	public String postTest(Member m) {
-		return "post request : "+ m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
+	public String postTest(@RequestBody String text) {
+		return "post request : "+text;
 	}
 	
 	@PutMapping("/http/put")
