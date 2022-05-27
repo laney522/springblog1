1bad60886fdbf5db73c13adffa44e08209659a30
laney522 <pm72222@gmail.com>
2022-05-27 17:43:59 +0900

    create User Table test2

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -2,8 +2,16 @@ package com.cos.blog.model;
 
 import java.sql.Timestamp;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity	// User 클래스가 스프링부트가 실행될때 User클래스를 읽어서 자동으로 MySQL에 테이블이 생성된다.
+public class User {
+	
+	@Id // Primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
 	private int id; // 시퀀스, auto_increment
 	
 	private String username; // 아이디
