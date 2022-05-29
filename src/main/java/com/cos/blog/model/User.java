84fd528ad3f9a1aa35258fb8b8475d8d711ec11f
laney522 <pm72222@gmail.com>
2022-05-29 23:16:35 +0900

    create user table test3

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -11,6 +11,7 @@ import javax.persistence.Id;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
+// ORM -> Java(다른언어) Object -> 테이블로 맵핑해주는 기술
 @Entity	// User 클래스가 스프링부트가 실행될때 User클래스를 읽어서 자동으로 MySQL에 테이블이 생성된다.
 public class User {
 	
