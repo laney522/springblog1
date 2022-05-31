d8169428418de7352053c7641426a049f34e01a0
laney522 <pm72222@gmail.com>
2022-05-31 22:57:19 +0900

    create Board table1

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,5 +1,34 @@
 package com.cos.blog.model;
 
-public class Board {
+import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+public class Board {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+	private int id;
+	
+	@Column(nullable = false, length = 100)
+	private String title;
+	
+	@Lob // 대용량 데이터
+	private String content; // 섬머노트 라이브러리 <html>태그가 섞여서 디자인이 됨.
+	
+	@ColumnDefault("0")
+	private int count; // 조회수
+	
+	private User user;// DB는 오브젝트를 저장할 수 없다.
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+	
 }
