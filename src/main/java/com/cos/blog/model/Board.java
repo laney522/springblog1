61718f2c4d0e94d16aebf617371729ed90c224ca
laney522 <pm72222@gmail.com>
2022-06-05 18:48:29 +0900

    FetchType

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,15 +1,18 @@
 package com.cos.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -44,9 +47,12 @@ public class Board {
 		@ColumnDefault("0")
 		private int count; // 조회수
 		
-		@ManyToOne // Many = Board, User = One 한명의 유저는 여러개의 게시글을 쓸 수 있다.
+		@ManyToOne(fetch = FetchType.EAGER) // Many = Board, User = One 한명의 유저는 여러개의 게시글을 쓸 수 있다.
 		@JoinColumn(name="userId") 
-		private User user;// DB는 오브젝트를 저장할 수 없다. ORM 을 사용하기 때문에
+		private User user; // DB는 오브젝트를 저장할 수 없다. ORM 을 사용하기 때문에
+		
+		@OneToMany(mappedBy="board", fetch = FetchType.EAGER) // ( 난 FK가 아니에요) DB에 컬럼을 만들지 마세요,.
+		private List<Reply> reply;
 		
 		@CreationTimestamp
 		private Timestamp createDate; 
